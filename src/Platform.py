8701c3dd4e70282801d923bcb6ee9ada8d23685f
vyigityildiz <vahityigityildiz@outlook.com>
2023-04-20 23:21:48 +0300

    Added params for Platform class and a TODO.

--- src/Platform.py
+++ src/Platform.py
@@ -2,7 +2,8 @@ import pygame as pg
 
 from Obstacle import *
 
+#TODO: sprite directorysi belli olacak onu ayarla
 # Class that specifies the properties of platforms.
 class Platform(Obstacle):
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(self, x: int, y: int, sprite: str) -> None:
+        super().__init__(x, y, sprite)
