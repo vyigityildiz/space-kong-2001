b77cce37786abba1beaa84a88fe80f86e1038f26
vyigityildiz <vahityigityildiz@outlook.com>
2023-04-20 22:40:20 +0300

    Added an attribute to Obstacle class.

--- src/Obstacle.py
+++ src/Obstacle.py
@@ -2,5 +2,5 @@ import pygame as pg
 
 # General class for all obstacles; stairs, platforms, and oil drum.
 class Obstacle():
-    def __init__(self) -> None:
-        pass
+    def __init__(self, x, y) -> None:
+        self.pos = (x, y)
