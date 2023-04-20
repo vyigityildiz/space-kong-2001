d6ab47f1e4e19905eb33a6da543ac92cdb5cda58
vyigityildiz <vahityigityildiz@outlook.com>
2023-04-20 23:07:44 +0300

    Added descriptive comment for the constructor function of the Obstacle class.

--- src/Obstacle.py
+++ src/Obstacle.py
@@ -2,5 +2,13 @@ import pygame as pg
 
 # General class for all obstacles; stairs, platforms, and oil drum.
 class Obstacle():
-    def __init__(self, x, y) -> None:
+    """Class constructor
+
+    Args:
+        x pixel x coordinate value
+        y pixel y coordinate value
+        sprite directory of the sprite png
+    """
+    def __init__(self, x: int, y: int, sprite: str) -> None:
         self.pos = (x, y)
+        self.sprite = sprite
