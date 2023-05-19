ff95c82a33a3ae3b29624aa12f97403dfa4bb9d4
Yiğit <vahityigityildiz@outlook.com>
2023-05-20 00:19:47 +0300

    Handled starting methods.

--- src/Game.py
+++ src/Game.py
@@ -4,6 +4,7 @@ import pygame as pg
 class Game():
     def __init__(self) -> None:
         self.level = Level()
+        self.started = False
 
     def climb_down(self):
         pass
@@ -33,14 +34,25 @@ class Game():
         pass
 
     def start(self):
-        pass
+        self.started = True
 
     def render(self):
         pass
 
-    def tick(self, screen, frame):
+    def tick(self, screen, frame, events):
+        for event in events:
+            if event.type == pg.KEYDOWN:
+                self.start()
+
+        if not self.started:
+            pg.font.init()
+            font_to_use = pg.font.SysFont('arial', 50)
+            text_surface = font_to_use.render('Press any key to start.', False, (255, 255, 255))
+            screen.blit(text_surface, (0,0))
+
         self.level.draw_obstacles(screen)
-        self.level.draw_animated_instances(screen, frame)
+        if self.started:
+            self.level.draw_animated_instances(screen, frame)
 
     def _set_tick_speed(self):
         pass
--- src/main.py
+++ src/main.py
@@ -11,13 +11,14 @@ running = True
 frame = 0
 tick = 0
 while running:
-    for event in pg.event.get():
+    events = pg.event.get()
+    for event in events:
         if event.type == pg.QUIT:
             running = False
             
     screen.fill("gray6") # TODO: after done with level design change the color back to gray6
 
-    game.tick(screen, frame)
+    game.tick(screen, frame, events)
 
     tick += 1
     if tick % 8 == 0:
